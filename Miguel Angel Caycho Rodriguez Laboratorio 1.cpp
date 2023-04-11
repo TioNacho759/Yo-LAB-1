e5e5c4b31bf70cd7bd736dcf18d67d2b801345e2
TioNacho759 <130485277+TioNacho759@users.noreply.github.com>
2023-04-11 10:39:04 -0500

    Add files via upload

---
+++ Miguel Angel Caycho Rodriguez Laboratorio 1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Miguel Angel Caycho Rodriguez\n";
+    cout<<"Estudiante 2023-I\n";
+    cout<<"Buscaminas pro-player\n";
+return 0;
+}
+
